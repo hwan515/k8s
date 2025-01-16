a0a8ccd6daa9f9989e61663a2757e62e5bdcdb4d
hwan515 <142598961+hwan515@users.noreply.github.com>
2025-01-16 15:48:26 +0900

    Update 05-calico-installation.sh

--- 05-calico-installation.sh
+++ 05-calico-installation.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-cat <<EOF> calico-quay-crd.yaml
----
-apiVersion: operator.tigera.io/v1
-kind: Installation
-metadata:
-  name: default
-spec:
-  calicoNetwork:
-    bgp: Enabled
-    hostPorts: Enabled
-    ipPools:
-      - blockSize: 26
-        cidr: 192.168.0.0/16
-        encapsulation: IPIPCrossSubnet
-        natOutgoing: Enabled
-        nodeSelector: all()
-  registry: quay.io
-EOF
-
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 kubectl create namespace tigera-operator
 helm install calico projectcalico/tigera-operator --version v3.27.5 --namespace tigera-operator
 
-kubectl taint node node1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
+### controller 아래 명령어를 실행하면 컨트롤러 노드에도 pod가 생성됨
+# kubectl taint node node1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
 
 kubectl apply -f calico-quay-crd.yaml
 kubectl -n calico-system get pod 
