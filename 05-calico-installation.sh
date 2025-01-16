2420e125d9dc1adbad3cb6f255b82c9dc53112c9
hwan515 <142598961+hwan515@users.noreply.github.com>
2025-01-16 15:49:12 +0900

    Update 05-calico-installation.sh

--- 05-calico-installation.sh
+++ 05-calico-installation.sh
@@ -8,4 +8,3 @@ helm install calico projectcalico/tigera-operator --version v3.27.5 --namespace 
 # kubectl taint node node1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
 
 kubectl apply -f calico-quay-crd.yaml
-kubectl -n calico-system get pod 
